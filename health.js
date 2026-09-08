211e8ed2559353bbc36c4604e00a9b3d0433ce17
sitestestes42 <flavioruas118@gmail.com>
2026-09-07 22:30:28 -0300

    Delete health.js

--- health.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store");
-  return res.status(200).json({
-    ok: true,
-    provider: "geoapify",
-    configured: Boolean(process.env.GEOAPIFY_API_KEY)
-  });
-};
